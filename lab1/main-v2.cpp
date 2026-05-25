d271cfb8ae55fe1e51b3cad10ee4f1535c532bf2
Yanchenko Sergey <144523679+ZaAnimeTyanok@users.noreply.github.com>
2026-05-25 11:10:28 +0700

    Update main-v2.cpp

--- lab1/main-v2.cpp
+++ lab1/main-v2.cpp
@@ -5,10 +5,7 @@
 
 using namespace std;
 
-#define EXCHANGE_AX_DATA_TAG 1
-#define STOP_SIGNAL_TAG 2
-#define CONTINUE_SIGNAL_TAG 3
-#define EXCHANGE_X_DATA_TAG 4
+#define ROUND_SHARING_TAG 1
 
 class ProcessManager {
 private:
@@ -18,8 +15,8 @@ private:
     int row_count;
     int start_row;
     
-    double tau = 1e-3;
-    double eps = 1e-5;
+    double tau = 1e-4;
+    double eps = 1e-4;
 
     int final_iterations = 0;
     double final_time = 0.0;
@@ -27,112 +24,96 @@ private:
     std::vector<int> all_start_rows;
     std::vector<int> all_row_counts;
     
+    int next;
+    int prev;
+    
     std::vector<double> local_x;
     std::vector<double> local_b; 
     std::vector<double> local_Ax;     
     
-    double vector_norm(const std::vector<double>& v) {
-        double sum = 0;
-        for (double val : v) {
-            sum += val * val;
-        }
-        return std::sqrt(sum);
-    }
-    
-    std::vector<double> gather_full_vector(const std::vector<double>& local_part) {
-        std::vector<double> full_vector(n);
-        
-        if (mpi_rank == 0) {
-            std::copy(local_part.begin(), local_part.end(), full_vector.begin() + start_row);
-        
-            for (int src = 1; src < mpi_size; src++) {
-                MPI_Recv(&full_vector[all_start_rows[src]], all_row_counts[src], MPI_DOUBLE,
-                         src, EXCHANGE_X_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
+    double vector_norm(const std::vector<double>& local_v) {
+        double global_sum;
+        double local_sum = 0.0;
 
-            for (int dest = 1; dest < mpi_size; dest++) {
-                MPI_Send(full_vector.data(), n, MPI_DOUBLE, dest, EXCHANGE_X_DATA_TAG, MPI_COMM_WORLD);
-            }
-            
-        } else {
-            MPI_Send(local_part.data(), row_count, MPI_DOUBLE, 0, EXCHANGE_X_DATA_TAG, MPI_COMM_WORLD);
-            
-            MPI_Recv(full_vector.data(), n, MPI_DOUBLE, 0, EXCHANGE_X_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        
-        return full_vector;
-    }
-    
-    std::vector<double> compute_local_Ax(const std::vector<double>& full_x) {
-        std::vector<double> result(row_count, 0.0);
-        
-        for (int i = 0; i < row_count; i++) {
-            int global_row = start_row + i;
-            
-            for (int j = 0; j < n; j++) {
-                result[i] += full_x[j] * (1 + (int)(j == global_row));
-            }
+        for (double x : local_v){
+            local_sum += x * x;
         }
 
-        return result;
+        MPI_Allreduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        
+        return std::sqrt(global_sum);
+    }
+    
+    void round_sharing(std::vector<double>& local_v, int new_size){
+        if (mpi_size <= 1){
+            return;
+        } 
+
+        std::vector<double> new_v(new_size);
+
+        if (mpi_rank %2 == 0){
+            MPI_Send(local_v.data(), local_v.size(), MPI_DOUBLE, next, ROUND_SHARING_TAG, MPI_COMM_WORLD);
+            MPI_Recv(new_v.data(), new_size, MPI_DOUBLE, prev, ROUND_SHARING_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        else {
+            MPI_Recv(new_v.data(), new_size, MPI_DOUBLE, prev, ROUND_SHARING_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(local_v.data(), local_v.size(), MPI_DOUBLE, next, ROUND_SHARING_TAG, MPI_COMM_WORLD);
+        }
+
+        local_v = new_v;
+    }
+
+    void calculate_local_Ax() {
+        local_Ax = std::vector<double>(row_count, 0.0);
+        int offset = start_row;
+
+        for (int step = 0; step < mpi_size; step++){
+            for (int i = 0; i < row_count; i++) {
+                int global_row = start_row + i;
+
+                for (int j = 0; j < (int)local_x.size(); j++) {
+                    local_Ax[i] += local_x[j] * (1.0 + static_cast<double>(j + offset == global_row));
+                }
+            }
+
+            if (mpi_size > 1){
+                int prev_size = all_row_counts[(prev - step + mpi_size) % mpi_size];
+                offset = (offset + prev_size) % n;
+                
+                round_sharing(local_x, prev_size);
+            }
+        }
     }
     
     void run() {
-        std::vector<double> full_b = gather_full_vector(local_b);
-        double b_norm = vector_norm(full_b);
-        
         double start_time = MPI_Wtime();
         int iter = 0;
-        
-        MPI_Status status;
-        
+
+        const double b_norm = vector_norm(local_b);
+
         while (true) {
-            std::vector<double> full_x = gather_full_vector(local_x);
+            calculate_local_Ax();
             
-            local_Ax = compute_local_Ax(full_x);
+            std::vector<double> local_r(row_count);
+                
+            for (int i = 0; i < row_count; i++){
+                local_r[i] = local_Ax[i] - local_b[i];
+            }
+
+            double r_norm = vector_norm(local_r);
+            double res = r_norm / b_norm;
             
-            if (mpi_rank == 0) {
-                std::vector<double> full_Ax(n);
-                std::copy(local_Ax.begin(), local_Ax.end(), full_Ax.begin() + start_row);
-                
-                for (int src = 1; src < mpi_size; src++) {
-                    MPI_Recv(&full_Ax[all_start_rows[src]], all_row_counts[src], MPI_DOUBLE,
-                             src, EXCHANGE_AX_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                }
-                
-                std::vector<double> r_vector(n);
-                for (int i = 0; i < n; i++) {
-                    r_vector[i] = full_Ax[i] - full_b[i];
-                }
-                
-                double r_norm = vector_norm(r_vector);
-                
-                if (iter % 100 == 0) {
-                    std::cout << "Iter " << iter << ", residual = " << r_norm / b_norm << std::endl;
-                }
-                
-                if (r_norm / b_norm < eps) {
-                    for (int dest = 1; dest < mpi_size; dest++) {
-                        MPI_Send(NULL, 0, MPI_INT, dest, STOP_SIGNAL_TAG, MPI_COMM_WORLD);
-                    }
-                    break;
-                }
-                
-                for (int i = 0; i < row_count; i++) {
-                    int global_idx = start_row + i;
-                    local_x[i] -= tau * r_vector[global_idx];
-                }
+            if (iter % 1000 == 0 && mpi_rank == 0) {
+                std::cout << "Iterations " << iter << ", residual = " << res << ", time " << MPI_Wtime() - start_time << std::endl;
+            }
+
+            if (res < eps){
+                break;
+            }
             
-                
-            } else {
-                MPI_Send(local_Ax.data(), row_count, MPI_DOUBLE, 0, EXCHANGE_AX_DATA_TAG, MPI_COMM_WORLD);
-                
-                MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                
-                if (status.MPI_TAG == STOP_SIGNAL_TAG) {
-                    MPI_Recv(NULL, 0, MPI_INT, 0, STOP_SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    break;
-                }
+            for (int i = 0; i < row_count; i++) {
+                local_x[i] -= tau * local_r[i];
             }
             
             iter++;
@@ -152,30 +133,22 @@ public:
         int rows_per_proc = N / mpi_size;
         int remainder = N % mpi_size;
         
-        if (mpi_rank == 0) {
-            all_start_rows.resize(mpi_size);
-            all_row_counts.resize(mpi_size);
+        all_start_rows.resize(mpi_size);
+        all_row_counts.resize(mpi_size);
             
-            int offset = 0;
-            for (int i = 0; i < mpi_size; i++) {
-                all_start_rows[i] = offset;
-                all_row_counts[i] = rows_per_proc + (int)(i < remainder);
-                offset += all_row_counts[i];
-            }
-            
-            row_count = all_row_counts[0];
-            start_row = all_start_rows[0];
-        } 
-        
-        else {
-            row_count = rows_per_proc + (int)(mpi_rank < remainder);
-            int start = 0;
-            for (int i = 0; i < mpi_rank; i++) {
-                start += rows_per_proc + (int)(i < remainder);
-            }
-            start_row = start;
+        int offset = 0;
+        for (int i = 0; i < mpi_size; i++) {
+            all_start_rows[i] = offset;
+            all_row_counts[i] = rows_per_proc + (int)(i < remainder);
+            offset += all_row_counts[i];
         }
+            
+        row_count = all_row_counts[mpi_rank];
+        start_row = all_start_rows[mpi_rank];
         
+        next = (mpi_rank + 1) % mpi_size;
+        prev = (mpi_rank == 0) ? mpi_size - 1 : mpi_rank - 1;
+
         local_x = std::vector<double>(row_count, 0.0);
         local_b = std::vector<double>(row_count, 0.0);
     }
@@ -185,66 +158,70 @@ public:
     }
     
     void run_simple_test() {
-        for (int i = 0; i < row_count; i++) {
-            local_b[i] = n + 1.0;
-        }
+        local_b = std::vector<double>(row_count, n + 1);
+        local_x = std::vector<double>(row_count, 0.0);
         
         run();
+
         
+        double max_error = 0.0;
+        for (int i = 0; i < row_count; i++) {
+            max_error = std::max(max_error, std::fabs(local_x[i] - 1.0));
+        }
+
+        double global_max_error;
+        MPI_Allreduce(&max_error, &global_max_error, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
         if (mpi_rank == 0) {
-            std::vector<double> full_x = gather_full_vector(local_x);
-            
             std::cout << "\n========================================" << std::endl;
-            std::cout << "VARIANT 2: Vectors distributed" << std::endl;
+            std::cout << "LAB1 V2 SIMPLE TEST" << std::endl;
             std::cout << "========================================" << std::endl;
-            std::cout << "Converged in " << final_iterations << " iterations" << std::endl;
-            std::cout << "Time: " << final_time << " seconds" << std::endl;
-            
-            double max_error = 0.0;
-            for (int i = 0; i < n; i++) {
-                max_error = std::max(max_error, std::fabs(full_x[i] - 1.0));
-            }
-            
-            std::cout << "Max error: " << max_error << std::endl;
+            std::cout << "Process count:  " << mpi_size << std::endl;
+            std::cout << "Matrix size (N) = " << n << std::endl;
+            std::cout << "Iterations: " << final_iterations<< std::endl;
+            std::cout << "Max error: " << global_max_error << std::endl;
+            std::cout << "Total time: " << final_time << " seconds" << std::endl;
             std::cout << "========================================" << std::endl;
         }
     }
     
     void run_not_simple_test() {
-        std::vector<double> u(n);
-        for (int i = 0; i < n; i++) {
-            u[i] = std::sin(2.0 * M_PI * i / n);
-        }
-        
-        std::vector<double> full_u = u;
-        std::vector<double> local_b_temp(row_count, 0.0);
-        for (int i = 0; i < row_count; i++) {
-            int global_row = start_row + i;
-            for (int j = 0; j < n; j++) {
-                local_b_temp[i] += full_u[j] * (1 + (int)(j == global_row));
-            }
-        }
-        
+        local_x.resize(row_count);
+        local_b.resize(row_count);
 
-        local_b = local_b_temp;
+        std::vector<double> u(row_count);
+
+        for (int i = 0; i < row_count; i++) {
+            u[i] = std::sin(2.0 * M_PI * (start_row + i) / n);
+        }
+
+        local_x = u;
+        calculate_local_Ax();
         
+        local_b = local_Ax;
+        local_Ax.clear();
+
+        local_x.assign(row_count, 0.0);
+
         run();
-        
+
+        double max_error = 0.0;
+        for (int i = 0; i < row_count; i++) {
+            max_error = std::max(max_error, std::fabs(local_x[i] - u[i]));
+        }
+
+        double global_max_error;
+        MPI_Allreduce(&max_error, &global_max_error, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
         if (mpi_rank == 0) {
-            std::vector<double> full_x = gather_full_vector(local_x);
-            
             std::cout << "\n========================================" << std::endl;
-            std::cout << "VARIANT 2: NOT SIMPLE TEST (arbitrary solution)" << std::endl;
+            std::cout << "LAB1 V2 NOT SIMPLE TEST" << std::endl;
             std::cout << "========================================" << std::endl;
-            std::cout << "Converged in " << final_iterations << " iterations" << std::endl;
-            std::cout << "Time: " << final_time << " seconds" << std::endl;
-            
-            double max_error = 0.0;
-            for (int i = 0; i < n; i++) {
-                max_error = std::max(max_error, std::fabs(full_x[i] - u[i]));
-            }
-            
-            std::cout << "Max error: " << max_error << std::endl;
+            std::cout << "Process count:  " << mpi_size << std::endl;
+            std::cout << "Matrix size (N) = " << n << std::endl;
+            std::cout << "Iterations: " << final_iterations<< std::endl;
+            std::cout << "Max error: " << global_max_error << std::endl;
+            std::cout << "Total time: " << final_time << " seconds" << std::endl;
             std::cout << "========================================" << std::endl;
         }
     }
@@ -252,10 +229,10 @@ public:
 
 int main(int argc, char** argv) {
     try {
-        int N = 1000;
+        int N = 900;
         ProcessManager pm(argc, argv, N);
         
-        pm.run_simple_test();
+        // pm.run_simple_test();
         pm.run_not_simple_test();
         
     } 
